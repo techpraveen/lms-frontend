41e849885a9faed6aa4048b188a134c4e99e9171
Praveen Kumar Pandey <95954302+techpraveen@users.noreply.github.com>
2025-02-14 01:49:12 +0530

    Update vite.config.js
    version2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+      build: {
+    outDir: 'dist', // Output directory for the production build
+  },
     plugins: [react()],
     server: {
         proxy: {
@@ -13,3 +16,4 @@ export default defineConfig({
         },
     },
 });
+ 
